dc24c9bc4949eb3f652b104c2b9a5b63624c4301
satan-pro <sayantancreator@gmail.com>
2023-11-04 01:47:50 +0530

    Modified ToDo-list

--- ToDo-list/app.js
+++ ToDo-list/app.js
@@ -16,7 +16,6 @@ const itemSchema = new mongoose.Schema({
 });
 
 const dailyNote = mongoose.model("dailyNote", itemSchema);
-const workNote = mongoose.model('workNote', itemSchema);
 
 let note1 = new dailyNote({name:"Buy Food"});
 let note2 = new dailyNote({name:"cook"});
@@ -24,18 +23,22 @@ let note3 = new dailyNote({name: "eat"});
 
 let defaultNotes = [note1, note2, note3];
 
+const listSchema = new mongoose.Schema({
+    name: String,
+    items: [itemSchema]
+});
 
+const lists = mongoose.model("list", listSchema);
 
-let newNotes=["Buy food", "cook", "eat"];
-let workNotes=[];
+var today = new Date();
+let options = {
+    weekday:'long',
+    day: 'numeric',
+    month: 'long'
+};
+let currentDay = today.toLocaleDateString('en-US', options);
+
 app.get("/", function(req, res){
-    var today = new Date();
-    let options = {
-        weekday:'long',
-        day: 'numeric',
-        month: 'long'
-    };
-    let currentDay = today.toLocaleDateString('en-US', options);
 
     dailyNote.find({}).exec().then(items=>{
         if(items.length===0)
@@ -47,39 +50,50 @@ app.get("/", function(req, res){
         }
         else
         {
-            res.render('lists',{listName: currentDay, notes: items , name:"Work"});
+            res.render('lists',{listName: currentDay, notes: items});
         }
     });
-    
 }); 
 
 app.post("/", function(req, res){
    let item = req.body.newNote;
-   if(req.body.submit==="Work List")
+   let listName = req.body.submit;
+   let newNote = new dailyNote({name:item});
+   console.log(listName);
+   if(listName===currentDay)
    {
-    var newWorkNote = new workNote({name:item});
-    newWorkNote.save();
-    res.redirect("/work");
+    newNote.save();
+    res.redirect('/');
    }
    else{
-    var newDailyNote = new dailyNote({name:item});
-    newDailyNote.save();
-    res.redirect("/");
+    lists.findOne({name:listName}).exec().then(foundList=>{
+        let foundListItem = foundList.items;
+        foundListItem.push(newNote);
+        lists.updateOne({name:listName}, {items:foundListItem}).exec();
+        res.redirect("/"+listName);
+    });
    }
-  
 });
 
-app.get("/work", function(req, res){
-    workNote.find({}).then(notes=>{
-        res.render('lists', {listName:"Work List", notes:notes , name:"Home"});
-    })
-    
-});
-
-app.post("/work", function(req, res){
-    console.log(req.body);
-    res.redirect("/work");
-});
+app.get("/:listName", function(req, res){
+   const listName = req.params['listName'];
+   lists.findOne({name:listName}).exec().then(foundList=>{
+    if(!foundList)
+    {
+        //Create a new list
+        const newList = new lists({
+            name:listName,
+            items:defaultNotes
+        });
+        newList.save();
+        res.redirect("/"+listName);
+    }
+    else{
+        //Show an existing list
+        res.render('lists', {listName:foundList.name, notes:foundList.items});
+    }
+   });
+})
 
 app.get("/about", function(req, res){
     res.render('about');
@@ -95,7 +109,13 @@ app.post("/redirect", function(req, res){
     {
         res.redirect("/");
     }
-})
+});
+
+app.post("/delete", function(req, res){
+    let noteID = req.body.checkbox;
+    dailyNote.deleteOne({_id:noteID}).exec();
+    res.redirect('/');
+});
 
 app.listen(3000, function(){
     console.log("Server set on port 3000");
